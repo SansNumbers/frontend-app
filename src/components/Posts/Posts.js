7fc976fe310e1fe005933e2c00f09c6402f1fbb0
Ihor Antypin <iantypin@c1r6p3.khpi.ucode-connect.study>
2021-06-16 16:27:37 +0300

    pagination

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 
 import '../../App.css';
 
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,50 +1,88 @@
-import React from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 import '../../App.css';
 import './Users.css';
 import { Link } from 'react-router-dom';
 
 export default class PersonList extends React.Component {
-  state = {
-    users: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      offset: 0,
+      data: [],
+      perPage: 12,
+      currentPage: 0
+    };
+    this.handlePageClick = this
+    .handlePageClick
+    .bind(this);
   }
+
+  receivedData() {
+    axios
+      .get(`http://127.0.0.1:8000/api/users`)
+      .then(res => {
+
+        const users = res.data.data;
+        console.log(res);
+        const slicing = users.slice(this.state.offset, this.state.offset + this.state.perPage)
+        const postData = slicing.map(person => <React.Fragment>
+        <div class="box--container">
+          <div class={"boxes"}>
+            <div class="login">Login: {person.login}</div>
+            <div class="name">Name: {person.name}</div>
+            <div class="rating">Rating: {person.rating}</div>
+            <div class="avatar"> 
+              <img  class="avatarpic" src={'http://localhost:8000/' + person.avatar} alt="Image not found" />
+            </div>
+          {/* <span><Link to={}>Go to User</Link></span> */}
+         </div>
+        </div>
+        </React.Fragment>)
+
+        this.setState({
+          pageCount: Math.ceil(users.length / this.state.perPage),
+          postData
+      })
+    });
+  }
+
+  handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    const offset = selectedPage * this.state.perPage;
+
+    this.setState({
+        currentPage: selectedPage,
+        offset: offset
+    }, () => {
+        this.receivedData()
+    });
+  };
 
   componentDidMount() {
-    axios.get(`http://127.0.0.1:8000/api/users`)
-      .then(res => {
-        const users = res.data;
-        this.setState({ users });
-      })
+    this.receivedData()
   }
 
-  render() 
-  {
+  render() {
     return(
-      <>
       <div class="box--container-wrapper">
         <h1>Users </h1>
-       
-        <div class="box--container">
-        {
-          this.state.users.map((person, ind) =>{
-            const url = "/users/" + person.id
-            return (
-              <div class={"boxes"}>
-
-                <div class="login" key={ind}>Login: {person.login}</div>
-                <div class="name" key={ind}>Name: {person.name}</div>
-                <div class="rating" key={ind}>Rating: {person.rating}</div>
-                <div class="avatar" key={ind}> 
-                  <img  class="avatarpic" src={'http://localhost:8000/' + person.avatar} alt="Image not found" />
-                </div>
-                <span><Link to={url}>Go to User</Link></span>
-              </div>
-            );
-          })
-        }
-        </div>
+            {this.state.postData}
+            <ReactPaginate
+              previousLabel={"prev"}
+              nextLabel={"next"}
+              breakLabel={"..."}
+              breakClassName={"break-me"}
+              pageCount={this.state.pageCount}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={this.handlePageClick}
+              containerClassName={"pagination"}
+              subContainerClassName={"pages pagination"}
+              activeClassName={"active"}/>
       </div>
-      </>
     );
   }
 }
